20a53304baf652b692dd8f65ba8d410272daee75
Doarakko <2wua4nlyi@gmail.com>
2019-07-05 15:05:14 +0900

    fix field size

--- backend/kagoole/models.py
+++ backend/kagoole/models.py
@@ -1,29 +1,33 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.postgres.fields import ArrayField
 
 
 class Competition(models.Model):
     # id
-    competition_id = models.PositiveIntegerField()
+    competition_id = models.PositiveIntegerField(unique=True)
     # ref
-    ref = models.CharField(max_length=50)
+    ref = models.CharField(max_length=100, unique=True)
     # title
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     # description
-    description = models.CharField(max_length=500)
+    description = models.TextField()
     # url
-    url = models.URLField(max_length=200)
+    url = models.URLField(max_length=200, unique=True)
 
     # category
     category = models.CharField(max_length=20)
     # evaluationMetric
-    evaluation_metric = models.CharField(max_length=30, null=True)
+    evaluation_metric = models.CharField(max_length=80, null=True)
+
     # tags
-    # tags = ArrayField(
-    #     models.CharField(max_length=20)
-    # )
-    # data_types = models.CharField(max_length=30)
-    # predict_type = models.CharField(max_length=10)
+    tags = ArrayField(
+        models.CharField(max_length=30)
+    )
+    data_types = ArrayField(
+        models.CharField(max_length=10)
+    )
+    predict_type = models.CharField(max_length=30, null=True)
 
     # isKernelsSubmissionsOnly
     is_kernel_only = models.BooleanField()
@@ -60,20 +64,15 @@ class Solution(models.Model):
         ('Nothing', 'Nothing'),
     ]
     medal = models.CharField(
-        max_length=5, choices=MEDAL_CHOICES)
+        max_length=10, choices=MEDAL_CHOICES)
 
     # top_rate = models.IntegerField()
     include_code = models.BooleanField()
     competition = models.ForeignKey(
         Competition, on_delete=models.CASCADE)
-    # related_name='competition',
 
     def __str__(self):
         return self.url
 
     # def save(self):
     #     top_rate = self.rank / self.competition.team_count
-
-
-class Tips(models.Model):
-    pass
