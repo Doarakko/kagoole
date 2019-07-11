378be6b33d2ad377bd591a44b466886e72a2f13a
Doarakko <2wua4nlyi@gmail.com>
2019-07-11 13:05:52 +0900

    delete useless field from filter and search fields

--- backend/kagoole/views.py
+++ backend/kagoole/views.py
@@ -2,54 +2,24 @@ from rest_framework import filters, generics, viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 
 
-from kagoole.models import Competition, Solution, Tips
-from kagoole.serializers import CompetitionSerializer, SolutionSerializer, TipsSerializer
+from kagoole.models import Competition, Solution
+from kagoole.serializers import CompetitionSerializer, SolutionSerializer
 
 
 class CompetitionList(viewsets.ReadOnlyModelViewSet):
     queryset = Competition.objects.all()
     serializer_class = CompetitionSerializer
 
-    filter_backends = (filters.SearchFilter, DjangoFilterBackend,)
-    filterset_fields = '__all__'
-    search_fields = (
-        'title',
-        'description',
-        'category',
-        'evaluation_metric',
-        'organization_name',
-    )
-
 
 class SolutionList(generics.ListCreateAPIView):
     queryset = Solution.objects.all()
     serializer_class = SolutionSerializer
-    filter_backends = (filters.SearchFilter, DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend,)
     filterset_fields = (
-        'rank',
-        'medal',
-        'competition__title',
-        'competition__description',
-        'competition__category',
-        'competition__evaluation_metric',
-        'competition__organization_name',
-        'competition__can_get_award_points',
-    )
-    search_fields = (
-        'medal',
-        'competition__title',
-        'competition__description',
-        'competition__category',
-        'competition__evaluation_metric',
-        'competition__organization_name',
+        'url',
     )
 
 
 class SolutionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Solution.objects.all()
-    serializer_class = SolutionSerializer
-
-
-class TipsList(generics.ListCreateAPIView):
-    queryset = Tips.objects.all()
     serializer_class = SolutionSerializer
