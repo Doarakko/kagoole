4913d0e9929a0cfa04275fc558c93985b7cf89b6
Doarakko <2wua4nlyi@gmail.com>
2019-07-21 20:17:36 +0900

    introduce formik

--- frontend/src/modal/SaveModal.js
+++ frontend/src/modal/SaveModal.js
@@ -1,24 +1,151 @@
 import React from "react";
 import { Button, Form, Modal } from "react-bootstrap";
+import { withFormik } from "formik";
+import * as yup from "yup";
 import axios from "axios";
 import Select from 'react-select';
 
 
-class SaveModal extends React.Component {
+const MyForm = props => {
+    const {
+        values,
+        touched,
+        errors,
+        isValid,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        setFieldValue,
+        setFieldTouched,
+        isSubmitting
+    } = props;
+
+    return (
+        <Form
+            onSubmit={handleSubmit}
+        >
+            <CompetitionSelect
+                value={values.competition}
+                onChange={setFieldValue}
+                onBlur={setFieldTouched}
+                error={errors.competition}
+                touched={touched.competition}
+            />
+            <Form.Group>
+                <Form.Label>Rank</Form.Label>
+                <Form.Control
+                    type="number"
+                    placeholder="Private Leaderboard Rank"
+                    name='rank'
+                    id='rank'
+                    value={values.rank}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                />
+                {errors.rank && touched.rank && (
+                    <div style={{ color: "red", marginTop: ".5rem" }}>{errors.rank}</div>
+                )}
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Solution URL</Form.Label>
+                <Form.Control
+                    type="url"
+                    placeholder="https://www.abc.com/xyz"
+                    name='solutionUrl'
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                />
+                {errors.solutionUrl && touched.solutionUrl && (
+                    <div style={{ color: "red", marginTop: ".5rem" }}>{errors.solutionUrl}</div>
+                )}
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Medal</Form.Label>
+                <Form.Control
+                    as="select"
+                    name='medal'
+                    type="text"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.medal}
+                >
+                    <option>Gold</option>
+                    <option>Silver</option>
+                    <option>Bronze</option>
+                    <option>Nothing</option>
+                </Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Include Code</Form.Label>
+                <Form.Check
+                    type="checkbox"
+                    name='includeCode'
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.includeCode}
+                />
+            </Form.Group>
+            <Modal.Footer>
+                <Button
+                    type="submit"
+                    variant="primary"
+                    disabled={isSubmitting}
+                    onClick={isValid && props.onHide}
+                >
+                    Save
+            </Button>
+            </Modal.Footer>
+        </Form >
+    );
+};
+
+
+const formikEnhancer = withFormik({
+    validationSchema: yup.object().shape({
+        competition: yup.object()
+            .required("Competition is required"),
+        rank: yup.number()
+            .positive("Rank must be a positive number")
+            .integer()
+            .required("Rank is required"),
+        solutionUrl: yup.string()
+            .url("Solution URL must be a valid URL")
+            .required("Solution URL is required"),
+    }),
+
+    mapPropsToValues: props => ({
+        competition: "",
+        rank: "",
+        solutionUrl: "",
+        medal: "Gold",
+        includeCode: false,
+    }),
+
+    handleSubmit: (values) => {
+        axios
+            .post("solutions/", {
+                competition: values.competition.ref,
+                rank: values.rank,
+                url: values.solutionUrl,
+                medal: values.medal,
+                include_code: values.includeCode,
+            }, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
+                }
+            )
+            .catch(err => console.log(err));
+    },
+});
+
+
+class CompetitionSelect extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             competitionList: [],
-
-            competition: null,
-            rank: null,
-            solutionUrl: null,
-            medal: 'Gold',
-            includeCode: false,
         };
-
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
     };
 
     componentDidMount() {
@@ -32,31 +159,45 @@ class SaveModal extends React.Component {
             .catch(err => console.log(err));
     };
 
-    async handleSubmit(event) {
-        axios
-            .post("solutions/", {
-                competition: this.state.competition.ref,
-                rank: this.state.rank,
-                url: this.state.solutionUrl,
-                medal: this.state.medal,
-                include_code: this.state.includeCode,
-            }, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                    }
-                }
-            )
-            .catch(err => console.log(err));
+    handleChange = value => {
+        this.props.onChange("competition", value);
     };
 
-    handleCompetitionChange = competition => {
-        this.setState({ competition });
+    handleBlur = () => {
+        this.props.onBlur("competition", true);
     };
 
-    handleChange(event) {
-        this.setState({ [event.target.name]: event.target.value });
-    };
+    render() {
+        return (
+            <Form.Group>
+                <Form.Label>Competition</Form.Label>
+                <Select
+                    id='competition'
+                    name="competition"
+                    onChange={this.handleChange}
+                    onBlur={this.handleBlur}
+                    value={this.props.value}
+                    options={this.state.competitionList}
+                    getOptionLabel={option => option['title'] + ': ' + option['ended_at'].slice(0, 10)}
+                    getOptionValue={option => option['ref']}
+                    noOptionsMessage={() => 'No competition'}
+                    placeholder='Enter competition name'
+                    className="basic-single"
+                />
+                {!!this.props.error && this.props.touched && (
+                    <div style={{ color: "red", marginTop: ".5rem" }}>
+                        {this.props.error}
+                    </div>
+                )}
+            </Form.Group>
+        );
+    }
+}
 
+const MyEnhancedForm = formikEnhancer(MyForm);
+
+class SaveModal extends React.Component {
+    MyEnhancedForm = formikEnhancer(MyForm);
     render() {
         return (
             <Modal
@@ -72,67 +213,7 @@ class SaveModal extends React.Component {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <Form
-                        onSubmit={this.handleSubmit}
-                    >
-                        <Form.Group>
-                            <Form.Label>Competition</Form.Label>
-                            <Select
-                                name="ref"
-                                value={this.state.ref}
-                                onChange={this.handleCompetitionChange}
-                                options={this.state.competitionList}
-                                getOptionLabel={option => option['title'] + ': ' + option['ended_at'].slice(0, 10)}
-                                getOptionValue={option => option['ref']}
-                                noOptionsMessage={() => 'No competition'}
-                                placeholder='Enter competition name'
-                                className="basic-single"
-                            />
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Rank</Form.Label>
-                            <Form.Control
-                                type="number"
-                                placeholder="Private Leaderboard Rank"
-                                name='rank'
-                                onChange={this.handleChange}
-                                required
-                            />
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Solution URL</Form.Label>
-                            <Form.Control
-                                type="url"
-                                placeholder="https://www.abcd.com/xyz"
-                                name='solutionUrl'
-                                onChange={this.handleChange}
-                                required
-                            />
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Medal</Form.Label>
-                            <Form.Control as="select" name='medal' onChange={this.handleChange} >
-                                <option>Gold</option>
-                                <option>Silver</option>
-                                <option>Bronze</option>
-                                <option>Nothing</option>
-                            </Form.Control>
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Include Code</Form.Label>
-                            <Form.Check type="checkbox" name='includeCode' onChange={this.handleChange} />
-                        </Form.Group>
-                        <Modal.Footer>
-                            <Button
-                                type="submit"
-                                variant="primary"
-                                onClick={this.props.onHide}
-                                disabled={!(this.state.competition && this.state.rank && this.state.solutionUrl && this.state.medal)}
-                            >
-                                Save
-                            </Button>
-                        </Modal.Footer>
-                    </Form>
+                    <MyEnhancedForm onHide={this.props.onHide} />
                 </Modal.Body>
             </Modal >
         );
