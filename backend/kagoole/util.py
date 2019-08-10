e1f1ae3b2d0c81a6f8bc808c3ba0bcd3eca1f731
Doarakko <2wua4nlyi@gmail.com>
2019-08-10 17:27:46 +0900

    add post twitter function

--- backend/kagoole/util.py
+++ backend/kagoole/util.py
@@ -2,8 +2,13 @@ import os
 import json
 import requests
 
+from requests_oauthlib import OAuth1Session
 
 SLACK_WEBHOOK_URL = os.environ['SLACK_WEBHOOK_URL']
+TWITTER_API_KEY = os.environ['TWITTER_API_KEY']
+TWITTER_API_SECRET = os.environ['TWITTER_API_SECRET']
+TWITTER_ACCESS_TOKEN = os.environ['TWITTER_ACCESS_TOKEN']
+TWITTER_ACCESS_TOKEN_SECRET = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
 
 
 def post_slack(title, value):
@@ -20,3 +25,14 @@ def post_slack(title, value):
         }]
     }
     requests.post(SLACK_WEBHOOK_URL, data=json.dumps(payload))
+
+
+def post_twitter(message):
+    endpoint = 'https://api.twitter.com/1.1/'
+    url = endpoint + 'statuses/update.json'
+
+    params = {"status": message}
+    session = OAuth1Session(TWITTER_API_KEY, TWITTER_API_SECRET,
+                            TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+
+    request = session.post(url=url, params=params)
