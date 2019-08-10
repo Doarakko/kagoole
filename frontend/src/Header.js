6909016f1af060b770f153d5a2050eb457c5ccfa
Doarakko <2wua4nlyi@gmail.com>
2019-08-10 15:52:31 +0900

    fix css and add twitter link

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -8,27 +8,25 @@ import SaveModalButton from './modal/SaveModalButton';
 class Header extends React.Component {
     render() {
         return (
-            <Navbar bg="primary" variant="dark" expand="lg" className="header">
+            <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
                 <Navbar.Brand>Kagoole</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mb-2 mr-sm-2 mb-sm-0">
-                        <Link to="/competitions">Competition</Link>
+                        <Link to="/competitions" style={{ color: "white" }}>Competition</Link>
                     </Nav>
                     <Nav className="mb-2 mr-sm-2 mb-sm-0">
-                        <Link to="/solutions">Solution</Link>
+                        <Link to="/solutions" style={{ color: "white" }}>Solutions</Link>
                     </Nav>
-                    <Nav className="mb-2 mr-sm-2 mb-sm-0">
+                    <Nav className="mr-auto">
                         <SaveModalButton buttonText='Add Solution' />
                     </Nav>
-                    <Nav className="mb-2 mr-sm-2 mb-sm-0 navbar-collapse justify-content-end">
-                        <a href="https://docs.google.com/forms/d/e/1FAIpQLSeZlhz5UK92LLY126uBaKXIl8AstQpaxlPhWcC8aHO2SE5FeQ/viewform?usp=sf_link">Contact</a>
-                    </Nav>
                     <Nav>
-                        <a href="https://github.com/Doarakko/kagoole">GitHub</a>
+                        <Nav.Link href="https://twitter.com/kagoole" style={{ color: "white" }}>Twitter</Nav.Link>
+                        <Nav.Link href="https://github.com/Doarakko/kagoole" style={{ color: "white" }}>GitHub</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
-            </Navbar >
+            </Navbar>
         );
     }
 }
